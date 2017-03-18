bbc26ee22e8d6d793560c84a4af924644bfb2ab3
user <user@mini-7.dlink>
2017-03-18 08:32:54 +0400

    Initial Commit

---
+++ Recipes Sample/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Recipes Sample
+//
+//  Created by user on 18.03.17.
+//  Copyright © 2017 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Recipes Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Recipes Sample
+//
+//  Created by user on 18.03.17.
+//  Copyright © 2017 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
