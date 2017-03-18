e318caaa4a11abd8901f2a278b7337842df94bd3
user <user@mini-7.local>
2017-03-18 09:54:03 +0400

    Init commit with one network request.

--- Recipes Sample/ViewController.h
+++ Recipes Sample/ViewController.h
@@ -9,6 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    NSMutableData *_responseData;
+}
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *progressBar;
 
 
 @end
--- Recipes Sample/ViewController.m
+++ Recipes Sample/ViewController.m
@@ -16,14 +16,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [_progressBar startAnimating];
+    
     // Do any additional setup after loading the view, typically from a nib.
-}
+    NSString *urlString = [NSString stringWithFormat: @"http://food2fork.com/api/search?key=74aa18f5cda819ae69e51504ca5e6059"];
+    
+    NSCharacterSet *set = [NSCharacterSet URLQueryAllowedCharacterSet];
+    NSString *encodedUrlAsString = [urlString stringByAddingPercentEncodingWithAllowedCharacters:set];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    
+    [[session dataTaskWithURL:[NSURL URLWithString:encodedUrlAsString]
+            completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                
+                NSLog(@"RESPONSE: %@",response);
+                NSLog(@"DATA: %@",data);
+                
+                if (!error) {
+                    // Success
+                    if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+                        NSError *jsonError;
+                        NSDictionary *jsonResponse = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                        
+                        if (jsonError) {
+                            // Error Parsing JSON
+                            
+                        } else {
+                            // Success Parsing JSON
+                            // Log NSDictionary response:
+                            [self dataLoadedSuccessfully:jsonResponse];
+                        }
+                    }  else {
+                        //Web server is returning an error
+                    }
+                } else {
+                    // Fail
+                    NSLog(@"error : %@", error.description);
+                }
+            }] resume];}
 
+
+- (void)dataLoadedSuccessfully:(NSDictionary *)jsonData {
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
